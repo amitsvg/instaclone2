910796629eaab5bd21667fa18b1d744bc5d00a49
Amit Kumar <72488615+amitsvg@users.noreply.github.com>
2023-01-18 12:09:16 +0530

    backend files updated

--- server.js
+++ server.js
@@ -1,42 +1,42 @@
-// require('dotenv').config();
+require('dotenv').config();
 const path = require("path");
 const express = require('express');
 const app = express();
 const PORT = 5000;
 const cors = require('cors');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 app.use(cors());
-// require('./models/model')
-// require('./models/post')
-// app.use(express.json())
-// app.use(require("./routes/auth"))
-// app.use(require("./routes/createPost"))
-// app.use(require("./routes/user"))
+require('./models/model')
+require('./models/post')
+app.use(express.json())
+app.use(require("./routes/auth"))
+app.use(require("./routes/createPost"))
+app.use(require("./routes/user"))
 
-// mongoose.connect(mongoUrl, {useNewUrlParser: true});
-// const connectDB = async () => {
-//     try {
-//         const conn = await mongoose.connect(process.env.MONGO_URI);
-//         console.log(`MongoDB Connected: ${conn.connection.host}`);
-//     } catch (error) {
-//         console.log(error);
-//         process.exit(1);
-//     }
-// }
+mongoose.connect(mongoUrl, { useNewUrlParser: true });
+const connectDB = async () => {
+    try {
+        const conn = mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
 
-// connectDB().then(() => {
-    app.use(express.static(path.join(__dirname, "./client/build")));
-    app.get("*", function (_, res) {
-      res.sendFile(
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+    res.sendFile(
         path.join(__dirname, "./client/build/index.html"),
         function (err) {
-          res.status(500).send(err);
+            res.status(500).send(err);
         }
-      );
-    });
+    );
+});
 
-app.listen(PORT, () => {
-    console.log(`server si running on ${PORT}`);
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`server si running on ${PORT}`);
+    })
 })
-// })
